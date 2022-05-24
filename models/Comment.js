ebb25c0428d88fcbec8eb8a485cb442cb3142e1c
Deck Stream <tneswick2@verizon.net>
2022-05-24 14:33:59 -0700

    feature/comment-model

---
+++ models/Comment.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// create comment model from CommentSchema
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
